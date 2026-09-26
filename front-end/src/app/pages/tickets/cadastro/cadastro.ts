d792f5d3434d57e9ad3b352b9de5f38bdf3f4a73
Vitor Prange <12965@proway.treina>
2026-09-25 21:57:50 -0300

    feat: modais, listar, usuario

--- front-end/src/app/pages/tickets/cadastro/cadastro.ts
+++ front-end/src/app/pages/tickets/cadastro/cadastro.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TicketCadastro } from '../../../models/tickets.model';
+import { TicketService } from '../../../services/ticket.service';
+import { Router, RouterLink } from '@angular/router';
+import { UsuarioService } from '../../../services/usuario.service';
+import { UsuarioResposta } from '../../../models/usuarios.model';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
-export class Cadastro {}
+export class Cadastro {
+
+  ticketService = inject(TicketService);
+  usuarioService = inject(UsuarioService);
+  router = inject(Router);
+
+  usuarios = signal<UsuarioResposta[]>([]);
+
+  setores = [
+    {
+      "titulo": "TI",
+      "descricao": "TI"
+    },
+    {
+      "titulo": "RH",
+      "descricao": "Recursos Humanos"
+    },
+    {
+      "titulo": "FINANCEIRO",
+      "descricao": "Financeiro"
+    },
+    {
+      "titulo": "ADMINISTRATIVO",
+      "descricao": "Administrativo"
+    },
+    {
+      "titulo": "MANUTENCAO",
+      "descricao": "Manutenção"
+    }
+  ]
+
+  ticket: TicketCadastro = {
+    descricao: "",
+    idUsuario: null,
+    setor: "",
+    titulo: ""
+  }
+
+  ngOnInit() {
+    this.carregarUsuarios();
+  }
+
+  carregarUsuarios() {
+    this.usuarioService.listar().subscribe({
+      next: usuarios => this.usuarios.set(usuarios),
+      error: erro => {
+        console.error(erro);
+        alert("Não foi possivel listar os usuarios")
+      }
+    })
+  }
+
+  cadastrar(){
+    this.ticketService.cadastrar(this.ticket).subscribe({
+      next: () => {
+        alert("Ticket cadastrado com sucesso")
+        this.router.navigate(["/tickets"]);
+      },
+      error: erro => {
+        console.error(erro);
+        alert("Não foi possivel cadastrar o ticket");
+      }
+    })
+  }
+}
--- front-end/src/app/pages/tickets/listar/listar.ts
+++ front-end/src/app/pages/tickets/listar/listar.ts
@@ -1,22 +1,45 @@
 import { Component, inject, signal } from '@angular/core';
 import { TicketService } from '../../../services/ticket.service';
-import { TicketResposta } from '../../../models/tickets.model';
+import { TicketAssociar, TicketResposta } from '../../../models/tickets.model';
 import { RouterLink } from '@angular/router';
+import { Modal } from '../../../shared/modal/modal';
+import { UsuarioResposta } from '../../../models/usuarios.model';
+import { FormsModule } from '@angular/forms';
+import { UsuarioService } from '../../../services/usuario.service';
 
 @Component({
   selector: 'app-listar',
-  imports: [RouterLink],
+  imports: [RouterLink, Modal, FormsModule],
   templateUrl: './listar.html',
   styleUrl: './listar.scss',
 })
 export class Listar {
   ticketService = inject(TicketService);
+  usuarioService = inject(UsuarioService);
 
   tickets = signal<TicketResposta[]>([]);
+  modalAssociarAberta = signal<boolean>(false)
+  ticketAssociar: TicketAssociar = {
+    idUsuario: null
+  }
+  usuarios = signal<UsuarioResposta[]>([])
+  ticketSelecionado = signal<number | null>(null)
 
   ngOnInit(){
     this.carregarTickets();
+    this.carregarUsuarios();
   }
+
+  carregarUsuarios() {
+    this.usuarioService.listar().subscribe({
+      next: usuarios => this.usuarios.set(usuarios),
+      error: erro => {
+        console.error(erro);
+        alert("Não foi possivel listar os usuarios")
+      }
+    })
+  }
+
 
   carregarTickets(){
     this.ticketService.listar().subscribe({
@@ -24,6 +47,30 @@ export class Listar {
       error: (erro) => {
         console.error(erro)
         alert("Não foi possivel carregar os tickets");
+      }
+    })
+  }
+
+  abrirModalAssociar(ticketId: number) {
+    this.ticketSelecionado.set(ticketId);
+    this.modalAssociarAberta.set(true)
+  }
+
+  associar() {
+    this.ticketService.associar(this.ticketSelecionado()!, this.ticketAssociar).subscribe({
+      next: () => {
+        this.modalAssociarAberta.set(false);
+
+        this.ticketAssociar = {
+          idUsuario: null
+        }
+        
+        alert("Ticket associado com sucesso");
+        this.carregarTickets();
+      },
+      error: erro => {
+        console.error(erro);
+        alert("Não foi possivel associar o ticket")
       }
     })
   }
